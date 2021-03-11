5ba6d0da7d38b4397ec41c534c2d5282bf39ac2c
Javier Garcia <javiergarcferrer@gmail.com>
2021-03-11 17:37:58 -0400

    db first steps

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,23 @@ from models import Book, Author
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello, Welcome to the library!"}
+
+
+
 
 @app.get("/books/")
 async def get_books():
@@ -29,7 +43,23 @@ async def delete_book(book_id: int): ##delete database book
 
 
 
+
 @app.get("/authors")
 async def get_authors():
     return {"message": "here would be your author list"}
 
+@app.get("/author/{author_id}")
+async def get_author(author_id: str):
+    return {"author_id": author_id}                             ##get database book id
+
+@app.post("/author/")
+async def create_author(author: Author):    ##create book on database
+    author_dict = author.dict()
+
+@app.put("/author/{author_id}")
+async def update_author(author_id: str, author: Author): ##update database book
+    pass
+
+@app.delete("/author/{author_id}")
+async def delete_author(author_id: str): ##delete database book
+    pass
